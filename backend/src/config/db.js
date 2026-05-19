f0eb990b827dd07cd15c1cbf9b4a40c97e86db9d
Rodolfo Sarkis <requiao.rodolfo@gmail.com>
2026-05-18 22:01:50 -0300

    backend funcionando com mongodb e auth

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,17 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-async function connectDB() {
+const connectDB = async () => {
   try {
-    console.log("Tentando conectar ao MongoDB...");
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB conectado com sucesso");
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log('MongoDB conectado');
   } catch (error) {
-    console.error("Erro ao conectar ao MongoDB:", error.message);
-    process.exit(1); // Encerra o processo em caso de erro
+    console.error(error.message);
+    process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
